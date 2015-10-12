0c9b72a92976f5a5809daf80ff3060a0cab0377e
jordi92500 <jordi92500@hotmail.fr>
2015-10-11 23:19:51 -0500

    Coords to Weather fix

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,22 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    # Definition of the URL adress where to fetch the data
+    url_address = "https://api.forecast.io/forecast/79cdf91d3ddb9a62ed3d79ec50184b08/#{@lat},#{@lng}"
 
+    # Refactoring of data retrieved to an Hash
+    parsed_forecast_data = JSON.parse(open(url_address).read)
 
-    @current_temperature = "Replace this string with your answer."
+    # Retrieving forecast information from the data
+    @current_temperature = parsed_forecast_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_forecast_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_forecast_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_forecast_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_forecast_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
